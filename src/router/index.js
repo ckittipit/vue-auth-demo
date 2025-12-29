efdcf607939537acd3f4bf64111327e8e0022a63
Kittipit Chanthataweesup <nungirannger@Kittipits-MacBook-Pro.local>
2025-12-29 17:31:49 +0700

    fix base error 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 import RegisterPage from '../views/RegisterPage.vue'
 import LoginPage from '../views/LoginPage.vue'
 import SuccessPage from '../views/SuccessPage.vue'
@@ -12,16 +11,13 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
-// guard: ต้องมี token ถึงเข้า success ได้
 router.beforeEach((to) => {
   const token = localStorage.getItem('token')
-  if (to.meta.requiresAuth && !token) {
-    return '/login'
-  }
+  if (to.meta.requiresAuth && !token) return '/login'
 })
 
 export default router
