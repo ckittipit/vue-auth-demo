a38a002315c835615b6a62d7caf5bbcfbe6e28ee
Kittipit Chanthataweesup <nungirannger@Kittipits-MacBook-Pro.local>
2025-12-29 17:16:32 +0700

    fix base error

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'vue-auth-demo',
+  base: '/vue-auth-demo/',
   plugins: [vue(), tailwindcss()],
 })
